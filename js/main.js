12c7aaade773c131fa1e4879ae2ec2c1be329da9
nghiadt24 <nghiadinh2244@gmail.com>
2025-02-08 21:08:48 +0700

    up

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
     gsap.registerPlugin(ScrollTrigger,EaselPlugin)
     console.log("cai dit con me ao that day");
     // gsap code here!
+    
     // Initialize a new Lenis instance for smooth scrolling
 const lenis = new Lenis();
 
@@ -17,12 +18,19 @@ gsap.ticker.add((time) => {
 
 // Disable lag smoothing in GSAP to prevent any delay in scroll animations
 gsap.ticker.lagSmoothing(0);
-const ScaleEffect = gsap.to('.HeroVideo',{
-    scale: 1.2
+
+// HeroVideo
+const
+const ScaleEffect = gsap.fromTo('.HeroVideo',{
+    scale: 0.8,
+},
+{
+    scale:1,
+    duration: 1,
 })
  ScrollTrigger.create ({
         trigger: '.HeroVideoRow',
-        markers: true,
+        markers: false,
         start: "top center",
         end: "bottom bottom",
         scrub: true,
@@ -30,4 +38,42 @@ const ScaleEffect = gsap.to('.HeroVideo',{
         duration: 1,
     })
 
-   });
+// *****
+
+// AboutSection 
+const AboutTitleAni = gsap.fromTo('#AboutTitle', {
+    y: 20,
+    opacity: 0,
+},
+{
+    y: 0,
+    opacity: 1,
+    duration: 0.6,
+    stagger: 0.1,
+    ease: 'power4.out',
+})
+    ScrollTrigger.create ({
+        trigger: '.AboutSection',
+        markers: false,
+        start: "top 80%",
+        end: "center bottom",
+        animation: AboutTitleAni,
+        duration: 0.6,
+        scrub: true,
+    })
+
+const AboutDesSplit = new SplitType('#AboutDes');
+const AboutDesAni = gsap.to(AboutDesSplit.words, {
+    color:"#292929",
+    stagger: 0.1,
+})
+    ScrollTrigger.create ({
+        trigger: '.AboutSection',
+        markers: true,
+        start: "top 50%",
+        end: "70% bottom",
+        animation: AboutDesAni,
+        duration: 0.6,
+        scrub: true,
+    })
+});
